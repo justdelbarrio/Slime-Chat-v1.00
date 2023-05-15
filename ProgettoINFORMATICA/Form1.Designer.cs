bd6604cce7ae9737b89d68fe792be4dbad14b71a
coolest g <102225880+justdelbarrio@users.noreply.github.com>
2023-05-15 10:52:17 +0200

    Add files via upload

---
+++ ProgettoINFORMATICA/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace ProgettoINFORMATICA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textLocalPort = new System.Windows.Forms.TextBox();
+            this.textLocalIP = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textFriendsPort = new System.Windows.Forms.TextBox();
+            this.textFriendsIP = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listMessage = new System.Windows.Forms.ListBox();
+            this.textMessage = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textLocalPort);
+            this.groupBox1.Controls.Add(this.textLocalIP);
+            this.groupBox1.Font = new System.Drawing.Font("Noto Sans Georgian", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.groupBox1.Location = new System.Drawing.Point(12, 32);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(330, 159);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Client1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Noto Sans Georgian", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(20, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 30);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "PORT";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Noto Sans Georgian", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(20, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 30);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IP";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textLocalPort
+            // 
+            this.textLocalPort.Font = new System.Drawing.Font("Noto Sans Georgian", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textLocalPort.Location = new System.Drawing.Point(109, 93);
+            this.textLocalPort.Multiline = true;
+            this.textLocalPort.Name = "textLocalPort";
+            this.textLocalPort.Size = new System.Drawing.Size(168, 30);
+            this.textLocalPort.TabIndex = 1;
+            // 
+            // textLocalIP
+            // 
+            this.textLocalIP.Enabled = false;
+            this.textLocalIP.Font = new System.Drawing.Font("Noto Sans Georgian", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textLocalIP.Location = new System.Drawing.Point(109, 34);
+            this.textLocalIP.Multiline = true;
+            this.textLocalIP.Name = "textLocalIP";
+            this.textLocalIP.Size = new System.Drawing.Size(168, 30);
+            this.textLocalIP.TabIndex = 0;
+            this.textLocalIP.TextChanged += new System.EventHandler(this.textLocalIP_TextChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.Menu;
+            this.groupBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("groupBox2.BackgroundImage")));
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.textFriendsPort);
+            this.groupBox2.Controls.Add(this.textFriendsIP);
+            this.groupBox2.Font = new System.Drawing.Font("Noto Sans Georgian", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.groupBox2.Location = new System.Drawing.Point(396, 32);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(330, 159);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Client2";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Noto Sans Georgian", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(18, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 30);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "PORT";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Noto Sans Georgian", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(18, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 30);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "IP";
+            // 
+            // textFriendsPort
+            // 
+            this.textFriendsPort.Font = new System.Drawing.Font("Noto Sans Georgian", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textFriendsPort.Location = new System.Drawing.Point(111, 93);
+            this.textFriendsPort.Multiline = true;
+            this.textFriendsPort.Name = "textFriendsPort";
+            this.textFriendsPort.Size = new System.Drawing.Size(168, 30);
+            this.textFriendsPort.TabIndex = 3;
+            // 
+            // textFriendsIP
+            // 
+            this.textFriendsIP.Font = new System.Drawing.Font("Noto Sans Georgian", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textFriendsIP.Location = new System.Drawing.Point(111, 34);
+            this.textFriendsIP.Multiline = true;
+            this.textFriendsIP.Name = "textFriendsIP";
+            this.textFriendsIP.Size = new System.Drawing.Size(168, 30);
+            this.textFriendsIP.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button1.Location = new System.Drawing.Point(752, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listMessage
+            // 
+            this.listMessage.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listMessage.FormattingEnabled = true;
+            this.listMessage.Location = new System.Drawing.Point(24, 239);
+            this.listMessage.Name = "listMessage";
+            this.listMessage.Size = new System.Drawing.Size(815, 195);
+            this.listMessage.TabIndex = 3;
+            this.listMessage.SelectedIndexChanged += new System.EventHandler(this.listMessage_SelectedIndexChanged);
+            // 
+            // textMessage
+            // 
+            this.textMessage.AcceptsReturn = true;
+            this.textMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textMessage.Location = new System.Drawing.Point(24, 454);
+            this.textMessage.Multiline = true;
+            this.textMessage.Name = "textMessage";
+            this.textMessage.Size = new System.Drawing.Size(580, 31);
+            this.textMessage.TabIndex = 4;
+            this.textMessage.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button2.Location = new System.Drawing.Point(639, 454);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 31);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Send";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LimeGreen;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(873, 507);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textMessage);
+            this.Controls.Add(this.listMessage);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Slime Chat";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textLocalPort;
+        private System.Windows.Forms.TextBox textLocalIP;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textFriendsPort;
+        private System.Windows.Forms.TextBox textFriendsIP;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listMessage;
+        private System.Windows.Forms.TextBox textMessage;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ProgettoINFORMATICA/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net;
+using System.Net.Sockets;
+
+namespace ProgettoINFORMATICA
+{
+    public partial class Form1 : Form
+    {
+        Socket sck;
+        EndPoint epLocal, epRemote;
+        public Form1()
+        {
+            InitializeComponent();
+            sck = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            sck.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            
+            textLocalIP.Text = GetLocalIP();
+            textFriendsIP.Text = GetLocalIP();
+        }
+
+        private string GetLocalIP()
+        {
+            IPHostEntry host;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+
+            foreach(IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            return "127.0.0.1";
+        }
+
+        private void MessageCallBack(IAsyncResult aRisultato)
+        {
+            try
+            {
+                int size = sck.EndReceiveFrom(aRisultato, ref epRemote);
+
+                if (size > 0) //checka se ci sono informazioni sul network o no
+                {
+                    byte[] datiRicevuti = new byte[1464];
+                     
+                    datiRicevuti = (byte[])aRisultato.AsyncState;
+
+                    ASCIIEncoding eEncoding = new ASCIIEncoding();
+                    string messaggioRicevuto = eEncoding.GetString(datiRicevuti); //converte datiRicevuti in string
+
+                    listMessage.Items.Add("Slime: "+messaggioRicevuto);
+                }
+                byte[] buffer = new byte[1500];
+                sck.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref epRemote, new AsyncCallback(MessageCallBack), buffer);
+            }
+            catch(Exception exp)
+            {
+                MessageBox.Show(exp.ToString());
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            groupBox1.BackColor = Color.Transparent;
+            groupBox2.BackColor = Color.Transparent;
+            groupBox1.ForeColor = Color.White;
+            groupBox2.ForeColor = Color.White;
+            label1.ForeColor = Color.White;
+            label2.ForeColor = Color.White;
+            label3.ForeColor = Color.White;
+            label4.ForeColor = Color.White;
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textLocalIP_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                epLocal = new IPEndPoint(IPAddress.Parse(textLocalIP.Text), Convert.ToInt32(textLocalPort.Text));
+                sck.Bind(epLocal);
+
+                epRemote = new IPEndPoint(IPAddress.Parse(textFriendsIP.Text), Convert.ToInt32(textFriendsPort.Text));
+                sck.Connect(epRemote);
+
+                byte[] buffer = new byte[1500];
+
+                sck.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref epRemote, new AsyncCallback(MessageCallBack), buffer);
+                
+                button1.Text = "Connected";
+                button1.Enabled = false;
+                button2.Enabled = true;
+                textMessage.Focus();
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Text.ASCIIEncoding enc = new System.Text.ASCIIEncoding();
+                byte[] msg = new byte[1500];
+                msg = enc.GetBytes(textMessage.Text);
+
+                sck.Send(msg);
+
+                listMessage.Items.Add("Tu: "+textMessage.Text);
+                textMessage.Clear();
+            }
+            catch(Exception exc)
+            {
+                MessageBox.Show(exc.ToString());
+            }
+        }
+
+        private void listMessage_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
